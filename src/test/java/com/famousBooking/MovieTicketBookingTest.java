756d72e4611d8c6a81cf24f8f02f2d632a857a45
Gaurav Sakhardande <gauravs841990@gmail.com>
2014-09-08 19:07:55 +0100

    Basic movie ticket booking test which took to other components

---
+++ src/test/java/com/famousBooking/MovieTicketBookingTest.java
@@ -0,0 +1,51 @@
+package com.famousBooking;
+
+import com.famousBooking.model.Booking;
+import com.famousBooking.model.Sms;
+import com.famousBooking.model.Theatre;
+import com.famousBooking.model.BookingSMS;
+import com.famousBooking.util.BookingSMSFormatValidator;
+import com.famousBooking.util.SeatNumberAllocator;
+import com.famousBooking.util.TicketAvailabilityChecker;
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * Created by gsakhardande on 4/9/14.
+ */
+public class MovieTicketBookingTest {
+
+    @Test
+    public void shouldBookTicketsSuccessfullyWithValidSms() {
+
+        String date = "26-AUG-2014";
+        String time = "11:00";
+        int numberOfTickets = 2;
+
+        long mobileNumber = 123;
+
+        List<Integer> expectedSeatNumbers = Arrays.asList(1, 2);
+
+        // Given
+        String message = "DT " + date + " " + time + " TKT " + numberOfTickets;
+        Sms sms = new Sms(message, mobileNumber);
+
+        Set<Booking> bookingRepository = Collections.emptySet();
+        Map<String, Integer> bookingStatsRepository = Collections.emptyMap();
+
+        BookingSMSFormatValidator validator = new BookingSMSFormatValidator(Theatre.BOOKING_SMS_FORMAT);
+        BookingSMS bookingSMS = new BookingSMS(sms, validator);
+
+        TicketAvailabilityChecker ticketAvailabilityChecker = new TicketAvailabilityChecker(bookingStatsRepository);
+
+        SeatNumberAllocator seatNumberAllocator = new SeatNumberAllocator(bookingRepository, bookingStatsRepository);
+
+
+        Theatre theatre = new Theatre(bookingRepository, bookingStatsRepository);
+
+        // When
+
+        // Then
+    }
+}
